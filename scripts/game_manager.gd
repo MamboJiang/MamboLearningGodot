ddace89aa8dcd308cf78eefc0b312c93694be66f
MamboJiang <mambojiang@icloud.com>
2024-06-16 15:31:15 +0800

    Have bug not fixed

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,12 +1,13 @@
 extends Node
 
 var score = 0
+var lives = 3
 
 @onready var score_label = $ScoreLabel
 
 func add_point():
 	score += 1
-	if score == 7:
+	if score == 8:
 		score_label.text = "You have collected all coins! Congrats!"
 	else:
-		score_label.text = "You have collected " + str(score) + " coins, " + str(7-score) + " coins left!"
+		score_label.text = "You have collected " + str(score) + " coins, " + str(8-score) + " coins left!"
